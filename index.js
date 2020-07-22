f7dfb226610d161ee70278d111bbb5d8037e19a2
werkon <werner.koenig.jr@gmail.com>
2020-07-22 12:44:52 +0200

    route

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
 const express = require('express')
 const app = express()
- 
-app.get('/', function (req, res) {
+
+app.use('/', express.static('preact'));
+
+app.get('/*', function (req, res) {
   res.send('Hello World')
 })
  
