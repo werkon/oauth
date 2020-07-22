f88ca96ded5207d98d68abfe8f4f088db51c04d7
werkon <werner.koenig.jr@gmail.com>
2020-07-22 13:14:52 +0200

    preact

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 
-app.use('/', express.static('preact'));
+app.use('/', express.static('preact/build'));
 
 app.get('/*', function (req, res) {
   res.send('Hello World')
